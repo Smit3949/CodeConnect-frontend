9e9439f44a2fb97e8d722c91b821a1afc620fe3b
vivek9patel <vivek.p9737@gmail.com>
2021-11-21 16:42:58 +0530

    chore: adds Auth0 Integration

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,22 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import { useAuth0 } from '@auth0/auth0-react'
+import { Login, Logout } from './auth/Auth0';
 
 export default function Preview({ docId }) {
+    const { isAuthenticated, user } = useAuth0();
     useEffect(() => {
         ReactGA.pageview('preview-screen');
     }, [])
+
+    console.log('isAuthenticated', isAuthenticated, user);
+
+    try {
+        if (isAuthenticated) localStorage.setItem('user', JSON.stringify(user));
+    }
+    catch (e) {
+        console.log(e);
+    }
 
     const joinRoomViaRoomId = () => {
         const roomId = document.getElementById("roomIdInput");
@@ -33,7 +45,7 @@ export default function Preview({ docId }) {
     return (
         <div className="bg-orange-standard select-none flex items-center justify-center h-full w-full">
             <div className="mb-20 flex flex-col items-center">
-                <div className="flex w-full text-white text-7xl font-bold codeFont justify-center">
+                <div className="flex w-full text-white text-3xl sm:text-7xl font-bold codeFont justify-center">
                     <span>&#60;CodeConnect &#47;&#62;</span>
                 </div>
                 <div className="flex flex-col mt-20 justify-center  text-white">
@@ -48,6 +60,12 @@ export default function Preview({ docId }) {
                     <div className="mt-10 flex">
                         <input id="roomIdInput" placeholder="Enter Room ID" type="text" className=" duration-300 rounded w-80 border-white focus:shadow-lg shadow-md text-black outline-none focus:outline-none px-4 py-3 codeFont" />
                         <button onClick={joinRoomViaRoomId} className="hover:shadow-lg duration-300 hover:bg-blue-700 px-4 ml-2 py-2 rounded-lg shadow bg-blue-600 font-medium">Join Room</button>
+                    </div>
+                    <div className="absolute bottom-24 flex justify-center items-center w-96 left-1/2 transform -translate-x-1/2">
+                        {
+                            !isAuthenticated ? <Login /> : <Logout />
+                        }
+
                     </div>
                 </div>
             </div>
